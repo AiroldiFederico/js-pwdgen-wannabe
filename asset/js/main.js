3b848781dd15140ca28ae2afff186f342bb09717
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-21 17:27:23 +0100

    FASE 1 - dichiaro le variabili in js

---
+++ asset/js/main.js
@@ -0,0 +1,18 @@
+
+
+
+
+// DICHIARAZIONI VARIABILI
+
+// nome utente
+const nome = undefined;
+
+// cognome utente
+const cognome = undefined;
+
+// colore preferito
+const colore = undefined;
+
+
+// pwd generator
+let pwdWeak = nome + cognome + colore + 21;
