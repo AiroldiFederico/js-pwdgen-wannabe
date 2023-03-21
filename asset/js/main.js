260ca9c13cbb5760ebf7b059809f13b3e3720c3e
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-21 18:22:53 +0100

    FASE 2 - tentativo 1  - errore output

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,14 +5,30 @@
 // DICHIARAZIONI VARIABILI
 
 // nome utente
-const nome = undefined;
+const nome = document.getElementById('name');
 
 // cognome utente
-const cognome = undefined;
+const cognome = document.getElementById('lstname');
 
 // colore preferito
-const colore = undefined;
+const colore = document.getElementById('color');
+
+// input btn
+const btn1 = document.getElementById('btn1');
+
+// output testo
+const output = document.getElementById('pwd');
 
 
 // pwd generator
-let pwdWeak = nome + cognome + colore + 21;
+const pwdweak = nome + cognome + colore + 21;
+
+
+
+// input by user
+
+function f1() {
+    output.innerText = pwdweak;
+}
+
+btn1.addEventListener('click',f1);
